bf3673a5d092c740594fae01a6e11a1bb933c282
mostvolubleplayer <jt.tocb@gmail.com>
2025-04-25 15:40:29 -0400

    boot_asteroids 0.01 - added constants.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # This line lets us use code from the open-source pygame lib throughout the file:
 import pygame
 
+# import the connect_database function
+# and the database_version variable
+# from database.py into the current file
+from database import connect_database, database_version
+
 def main():
 	print("Starting Asteroids!")
 	

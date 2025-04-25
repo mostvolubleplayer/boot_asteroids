9cb01e8794d5b959b0065476a2cf6cf694524233
mostvolubleplayer <jt.tocb@gmail.com>
2025-04-25 15:26:11 -0400

    boot_asteroids 0.01 - main.py only

---
+++ main.py
@@ -0,0 +1,8 @@
+# This line lets us use code from the open-source pygame lib throughout the file:
+import pygame
+
+def main():
+	print("Starting Asteroids!")
+	
+if __name__ == "__main__":
+		main()

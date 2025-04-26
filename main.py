cdfc5bb028092d287fbec8037e697901522d1323
mostvolubleplayer <jt.tocb@gmail.com>
2025-04-26 16:59:36 -0400

    boot_asteroids 0.23 - added FPS, checked against BOOTS

--- main.py
+++ main.py
@@ -16,9 +16,23 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
+	clock = pygame.time.Clock()
+	dt = 0
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				return
+		pygame.Surface.fill(screen, (0,0,0))
+		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
 		main()
-while True:
-	pygame.Surface.fill(screen, (0,0,0))
-	pygame.display.flip()
+
+
+
+
 

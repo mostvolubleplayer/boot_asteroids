9389a8182f26604c575771b78ad690228b812370
mostvolubleplayer <jt.tocb@gmail.com>
2025-04-26 16:00:02 -0400

    boot_asteroids 0.21 - add gitignore, black screen

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ import pygame
 # from database.py into the current file
 from constants import *
 
+#Use pygame's display.set_mode() to get a new GUI window:
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 def main():
+	pygame.init()
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
-	
 if __name__ == "__main__":
 		main()
+while True:
+	pygame.Surface.fill(screen, (0,0,0))
+	pygame.display.flip()
+

0df7695f7c43c2863028b8d39b8012038b20409e
mostvolubleplayer <jt.tocb@gmail.com>
2025-04-29 22:35:56 -0400

    boot_asteroids 0.28 - added groups

--- main.py
+++ main.py
@@ -17,21 +17,27 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
+	updatables = pygame.sprite.Group()
+	drawables = pygame.sprite.Group()
+	Player.containers = (updatables, drawables)
 	player = Player(SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2)
 	dt = 0
 
+
+
+# Below is the "GAME LOOP"
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		pygame.Surface.fill(screen, (0,0,0))
-		player.update(dt)
-
 		screen.fill("black")
-		player.draw(screen)
+		updatables.update(dt)
+		for d in drawables:
+			d.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
-
+# remember to label what is this for later understand:
 if __name__ == "__main__":
 		main()
